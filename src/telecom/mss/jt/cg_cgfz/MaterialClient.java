39a5c3573c9b37cf980d607abe1b91554c15abae
savy <xiaoyangsavy@163.com>
2017-09-05 16:21:32 +0800

    修改物料测试用例

--- src/telecom/mss/jt/cg_cgfz/MaterialClient.java
+++ src/telecom/mss/jt/cg_cgfz/MaterialClient.java
@@ -23,64 +23,25 @@ public class MaterialClient extends Thread {
 		// 创建服务视图
 		SIMDMSVRMATERIALINAsynService weatherInterfaceImplService = new SIMDMSVRMATERIALINAsynService();
 		// 获取服务实现类
-		SIMDMSVRMATERIALINAsyn weatherInterfaceImpl = weatherInterfaceImplService
-				.getPort(SIMDMSVRMATERIALINAsyn.class);
+		SIMDMSVRMATERIALINAsyn weatherInterfaceImpl = weatherInterfaceImplService.getPort(SIMDMSVRMATERIALINAsyn.class);
+		DTMATERIALCREATE.IREQUEST.MESSAGE message = this.getMessage();
+		DTBASEINFO baseInfo = this.getBaseInfo();
 		while (true) {
 			try {
 				Date d = new Date();
 				GregorianCalendar gc = new GregorianCalendar();
 				gc.setTime(d);
-//				if ((gc.get(gc.SECOND) == 0)) {
-				if ((gc.get(gc.SECOND) == 0) || (gc.get(gc.SECOND) == 30)) {
+				 if ((gc.get(gc.SECOND) == 0)) {
+//				if ((gc.get(gc.SECOND) == 0) || (gc.get(gc.SECOND) == 30)) {
 					i++;
 					DTMATERIALCREATE materialRoot = new DTMATERIALCREATE();
 					DTMATERIALCREATE.IREQUEST irequest = new DTMATERIALCREATE.IREQUEST();
 
-					DTBASEINFO baseInfo = new DTBASEINFO();
 					String dataString = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
 					Random random = new Random();
 					String msgId = "CG-TestMaterialCreate_MDM" + i + dataString + random.nextInt(10);
 					baseInfo.setMSGID(msgId);
-					baseInfo.setSENDTIME("11111111111111");
-					baseInfo.setSPROVINCE("99");
-					baseInfo.setSSYSTEM("CG-CGFZ");
-					baseInfo.setTSYSTEM("MDM");
-					baseInfo.setSERVICENAME("OP_MaterialCreate");
-					baseInfo.setTPROVINCE("99");
-					baseInfo.setRETRY("1");
 					irequest.setBASEINFO(baseInfo);
-					DTMATERIALCREATE.IREQUEST.MESSAGE message = new DTMATERIALCREATE.IREQUEST.MESSAGE();
-
-					List<DTMATERIALCREATE.IREQUEST.MESSAGE.ROOT> roots = new ArrayList<DTMATERIALCREATE.IREQUEST.MESSAGE.ROOT>();
-					DTMATERIALCREATE.IREQUEST.MESSAGE.ROOT root = new DTMATERIALCREATE.IREQUEST.MESSAGE.ROOT();
-					root.setZSQDID("000003");
-					List<DTMATERIALCREATE.IREQUEST.MESSAGE.ROOT.ITEM1> item1s = new ArrayList<DTMATERIALCREATE.IREQUEST.MESSAGE.ROOT.ITEM1>();
-					DTMATERIALCREATE.IREQUEST.MESSAGE.ROOT.ITEM1 item1 = new DTMATERIALCREATE.IREQUEST.MESSAGE.ROOT.ITEM1();
-					item1.setMATNR("J00000000003");
-					item1.setSTATUS("C");
-					item1.setMTART("WZ");
-					item1.setMAKTX("test");
-					item1.setMEINS("EA");
-					item1.setPRDHA("050609");
-					item1.setREMARK1("否");
-
-					List<ITEM2> item2s = new ArrayList<ITEM2>();
-					ITEM2 item2 = new ITEM2();
-					item2.setSTATUSPLANT("C");
-					item2.setZPROVINCE("00");
-					item2.setXCHPF("X");
-					item2.setBKLAS("3001");
-					item2.setQKLAS("6000");
-					item2.setZPURTYPE1("省采");
-					item2.setZSNACTIVEFLAG("X");
-					item2.setVABME("1");
-					item2s.add(item2);
-					item1.setItem2(item2s);
-
-					item1s.add(item1);
-					root.setItem1(item1s);
-					roots.add(root);
-					message.setRoot(roots);
 					irequest.setMESSAGE(message);
 					materialRoot.setIREQUEST(irequest);
 
@@ -98,10 +59,59 @@ public class MaterialClient extends Thread {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 1; i++) {
 			MaterialClient t = new MaterialClient();
 			t.start();
 		}
 
+	}
+
+	public DTMATERIALCREATE.IREQUEST.MESSAGE getMessage() {
+		DTMATERIALCREATE.IREQUEST.MESSAGE message = new DTMATERIALCREATE.IREQUEST.MESSAGE();
+
+		List<DTMATERIALCREATE.IREQUEST.MESSAGE.ROOT> roots = new ArrayList<DTMATERIALCREATE.IREQUEST.MESSAGE.ROOT>();
+		DTMATERIALCREATE.IREQUEST.MESSAGE.ROOT root = new DTMATERIALCREATE.IREQUEST.MESSAGE.ROOT();
+		root.setZSQDID("000003");
+		List<DTMATERIALCREATE.IREQUEST.MESSAGE.ROOT.ITEM1> item1s = new ArrayList<DTMATERIALCREATE.IREQUEST.MESSAGE.ROOT.ITEM1>();
+		DTMATERIALCREATE.IREQUEST.MESSAGE.ROOT.ITEM1 item1 = new DTMATERIALCREATE.IREQUEST.MESSAGE.ROOT.ITEM1();
+		item1.setMATNR("J00000000003");
+		item1.setSTATUS("C");
+		item1.setMTART("WZ");
+		item1.setMAKTX("test");
+		item1.setMEINS("EA");
+		item1.setPRDHA("050609");
+		item1.setREMARK1("否");
+
+		List<ITEM2> item2s = new ArrayList<ITEM2>();
+		ITEM2 item2 = new ITEM2();
+		item2.setSTATUSPLANT("C");
+		item2.setZPROVINCE("00");
+		item2.setXCHPF("X");
+		item2.setBKLAS("3001");
+		item2.setQKLAS("6000");
+		item2.setZPURTYPE1("省采");
+		item2.setZSNACTIVEFLAG("X");
+		item2.setVABME("1");
+		item2s.add(item2);
+		item1.setItem2(item2s);
+
+		item1s.add(item1);
+		root.setItem1(item1s);
+		roots.add(root);
+		message.setRoot(roots);
+
+		return message;
+	}
+
+	public DTBASEINFO getBaseInfo() {
+		DTBASEINFO baseInfo = new DTBASEINFO();
+		baseInfo.setSENDTIME("11111111111111");
+		baseInfo.setSPROVINCE("99");
+		baseInfo.setSSYSTEM("CG-CGFZ");
+		baseInfo.setTSYSTEM("MDM");
+		baseInfo.setSERVICENAME("OP_MaterialCreate");
+		baseInfo.setTPROVINCE("99");
+		baseInfo.setRETRY("1");
+		return baseInfo;
 	}
 }
